2afc28d47197d18928dc0daaaefb47f2c1e30057
Hitansh Kadakia <hitanshchiragkadakia@gmail.com>
2021-01-26 15:05:43 +0530

    Add files via upload

---
+++ userinput.py
@@ -0,0 +1,11 @@
+# Integer input from user
+a=int(input("Enter an integer"))
+print(a)
+
+# Float input from user
+b=float(input("Enter a float value"))
+print(b)
+
+# String input from user
+c=input("Enter any string")
+print(c)
