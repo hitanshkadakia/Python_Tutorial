c46f60b917efea52b7cf0de997ed5667b114f04d
Hitansh Kadakia <hitanshchiragkadakia@gmail.com>
2021-01-26 15:06:02 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,34 @@
+# Create a List
+List=["python","java","C++"]
+print(List)
+
+# access the lists items
+List=["python","java","C++"]
+print(List[1])
+
+# Negative indexing
+List=["python","java","C++"]
+print(List[-1])
+
+# Range of indexes
+List=["python","java","C++","kotlin"]
+print(List[1:3])
+
+# Change Item Value
+List=["python","java","C++"]
+List[1]="Flutter"
+print(List)
+
+# List Length
+List=["python","java","C++"]
+print(len(List))
+
+# Remove Item
+List=["python","java","C++"]
+List.remove("java")
+print(List)
+
+# Delete item
+List=["python","java","C++"]
+del List[0]
+print(List)
