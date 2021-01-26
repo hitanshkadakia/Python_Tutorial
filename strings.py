92d5311115981df0e3c9983c378d316fbe7df19d
Hitansh Kadakia <hitanshchiragkadakia@gmail.com>
2021-01-26 15:04:54 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,38 @@
+print('hello')
+# Output -> hello
+
+print("hello")
+# Output -> hello
+
+print("""hello""")
+# Output -> hello
+
+# Strings are array
+a="Hello, World!"
+print(a[1])
+# Output -> e
+
+# Slicing
+b="Hello, World!"
+print(b[2:5])
+# Output -> llo
+
+# Negative indexing
+c="Hello, World!"
+print(c[-5:-2])
+# Output -> orl
+
+# strip
+d="Hello, World!"
+print(d.strip())
+# Output -> Hello, World!
+
+# lower case
+e="Hello, World!"
+print(e.lower())
+# Output -> hello, world!
+
+# upper case
+f="Hello, World!"
+print(f.upper())
+# Output -> HELLO, WORLD!
