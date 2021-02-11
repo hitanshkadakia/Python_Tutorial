0e7f93e006e5fd04ad20f215d903fa7452aca65c
Hitansh Kadakia <hitanshchiragkadakia@gmail.com>
2021-02-11 18:15:04 +0530

    Add files via upload

---
+++ classobject.py
@@ -0,0 +1,61 @@
+#Create a Class
+
+class MyClass:
+  x = 5
+  
+#Create Object
+
+p1 = MyClass()
+print(p1.x)
+
+#The __init__() Function
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+#Object Methods
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+#The self Parameter
+
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+#Modify Object Properties
+
+p1.age = 40
+#Delete Object Properties
+
+del p1.age
+#Delete Objects
+
+del p1
+# The pass Statement
+
+class Person:
+  pass
